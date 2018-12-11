9bc66f6fcce785e250e389275d9bba563daba811
Diego Ferigo <diego.ferigo@iit.it>
2018-12-11 17:08:01 +0100

    Use script.sh in Travis CI also for Windows

--- .ci/script.sh
+++ .ci/script.sh
@@ -4,5 +4,11 @@ set -u
 
 cd $TRAVIS_BUILD_DIR
 mkdir -p build && cd build
-cmake -G"$TRAVIS_CMAKE_GENERATOR" -DCMAKE_BUILD_TYPE=$TRAVIS_BUILD_TYPE ..
-cmake --build . --config $TRAVIS_BUILD_TYPE
+
+if [ "$TRAVIS_CMAKE_GENERATOR" = "Visual Studio 15 2017 Win64" ] ; then
+    cmake -G"$TRAVIS_CMAKE_GENERATOR" ..
+    cmake --build . --config $TRAVIS_BUILD_TYPE
+else
+    cmake -G"$TRAVIS_CMAKE_GENERATOR" -DCMAKE_BUILD_TYPE=$TRAVIS_BUILD_TYPE ..
+    cmake --build .
+fi
