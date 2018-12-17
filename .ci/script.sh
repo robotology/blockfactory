0973acf2fd061021791aedec9162cacadbd54f0a
Diego Ferigo <diego.ferigo@iit.it>
2018-12-17 09:54:50 +0100

    Use the -A CMake option to select the architecture in Windows

--- .ci/script.sh
+++ .ci/script.sh
@@ -5,8 +5,8 @@ set -u
 cd $TRAVIS_BUILD_DIR
 mkdir -p build && cd build
 
-if [ "$TRAVIS_CMAKE_GENERATOR" = "Visual Studio 15 2017 Win64" ] ; then
-    cmake -G"$TRAVIS_CMAKE_GENERATOR" ..
+if [ "$TRAVIS_CMAKE_GENERATOR" = "Visual Studio 15 2017" ] ; then
+    cmake -G"$TRAVIS_CMAKE_GENERATOR" -A"${TRAVIS_CMAKE_ARCHITECTURE}" ..
     cmake --build . --config $TRAVIS_BUILD_TYPE
 else
     cmake -G"$TRAVIS_CMAKE_GENERATOR" -DCMAKE_BUILD_TYPE=$TRAVIS_BUILD_TYPE ..
