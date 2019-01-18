dfd6b8cb90627cffbc3e00b27c86d4375ec946c8
Diego Ferigo <diego.ferigo@iit.it>
2019-01-18 12:08:06 +0100

    Fix the compilation of the example

--- .ci/script.sh
+++ .ci/script.sh
@@ -12,6 +12,7 @@ if [ "$TRAVIS_CMAKE_GENERATOR" = "Visual Studio 15 2017" ] ; then
     cmake --build . --target INSTALL
     # Build the example
     cd $TRAVIS_BUILD_DIR/example
+    mkdir build && cd build
     cmake -G"$TRAVIS_CMAKE_GENERATOR" -A"${TRAVIS_CMAKE_ARCHITECTURE}" ..
     cmake --build . --config $TRAVIS_BUILD_TYPE
 else
@@ -21,6 +22,7 @@ else
     cmake --build . --target install
     # Build the example
     cd $TRAVIS_BUILD_DIR/example
+    mkdir build && cd build
     cmake -G"$TRAVIS_CMAKE_GENERATOR" -DCMAKE_BUILD_TYPE=$TRAVIS_BUILD_TYPE ..
     cmake --build .
 fi
