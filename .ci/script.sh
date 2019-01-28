29ceb0c6daa5b0098a0decb83347d0a1916fa916
Diego Ferigo <diego.ferigo@iit.it>
2019-01-28 14:58:38 +0100

    Fix Travis
    On xenial the Catch2 CMake functions fail if there is more than one test. This commit disables temporary ctest on xenial.

--- .ci/script.sh
+++ .ci/script.sh
@@ -18,7 +18,7 @@ if [ "$TRAVIS_CMAKE_GENERATOR" = "Visual Studio 15 2017" ] ; then
     cmake -G"$TRAVIS_CMAKE_GENERATOR" \
           -A"${TRAVIS_CMAKE_ARCHITECTURE}" \
           -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" \
-          -DBUILD_TESTING:BOOL=ON .. \
+          -DBUILD_TESTING:BOOL=${BUILD_TESTING:-0} .. \
           ..
     cmake --build . --config $TRAVIS_BUILD_TYPE
     cmake --build . --target INSTALL
@@ -32,7 +32,7 @@ else
     cmake -G"$TRAVIS_CMAKE_GENERATOR" \
           -DCMAKE_BUILD_TYPE=$TRAVIS_BUILD_TYPE \
           -DCMAKE_INSTALL_PREFIX="$INSTALL_PREFIX" \
-          -DBUILD_TESTING:BOOL=ON .. \
+          -DBUILD_TESTING:BOOL=${BUILD_TESTING:-0} .. \
           ..
     cmake --build .
     cmake --build . --target install
@@ -43,22 +43,27 @@ else
     cmake --build .
 fi
 
-# Run the unit tests with ctest
-cd $TRAVIS_BUILD_DIR/build
-ctest --output-on-failure
+if [ ${BUILD_TESTING:-0} -eq 1 ] ; then
 
-if [ "${VALGRIND_TESTS:-0}" -eq 1 ] ; then
+    # Run the unit tests with ctest
     cd $TRAVIS_BUILD_DIR/build
-    ctest -T memcheck --output-on-failure  || FAILED=1
+    ctest --output-on-failure
 
-    if [ ${FAILED:-0} -eq 1 ] ; then
-        for log in $(ls Testing/Temporary/MemoryChecker.*.log) ; do
-            echo
-            echo "$log"
-            echo
-            cat $log
-        done
-        exit 1
+    # Run the memcheck tests with ctest
+    if [ ${VALGRIND_TESTS:-0} -eq 1 ] ; then
+        cd $TRAVIS_BUILD_DIR/build
+        ctest -T memcheck --output-on-failure  || FAILED=1
+
+        # Print to stdout the output if memcheck fails
+        if [ ${FAILED:-0} -eq 1 ] ; then
+            for log in $(ls Testing/Temporary/MemoryChecker.*.log) ; do
+                echo
+                echo "$log"
+                echo
+                cat $log
+            done
+            exit 1
+        fi
     fi
 
 fi
